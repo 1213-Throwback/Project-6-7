8bb78eaeeb5256eba665a8f3723f85dcf52adebb
Vulcran <104237476+Vulcran@users.noreply.github.com>
2023-10-25 16:32:32 -0400

    I fully connected the mongoDB to the webserver.js and userDetail.jsx is now rendering properly

--- Project/06/components/userDetail/userDetail.jsx
+++ Project/06/components/userDetail/userDetail.jsx
@@ -14,7 +14,8 @@ class UserDetail extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        user:null
+        user:null,
+        userID: null
     };
   }
 
@@ -32,35 +33,42 @@ class UserDetail extends React.Component {
 
     componentDidUpdate() {
         const new_user_id = this.props.match.params.userId;
-        const current_user_id = this.state.user?._id;
+        const current_user_id = this.state.user._id;
         if (current_user_id  !== new_user_id){
             this.handleUserChange(new_user_id);
         }
     }
 
-    handleUserChange(user_id){
-        fetchModel("/user/" + user_id)
-            .then((response) =>
-            {
-                const new_user = response.data;
-                this.setState({
-                    user: new_user
+    handleUserChange(user_id) {
+        //if (user_id !== this.state.userID) {
+            fetchModel("/user/" + user_id)
+                .then((response) => {
+                    const new_user = response.data;
+                    console.log(new_user);
+                    console.log("write success");
+                    this.setState({
+                        user: new_user,
+                        userID: new_user._id
+                    });
+                    // Update main_content in state if needed
+                    const main_content = "User Details for " + new_user.first_name + " " + new_user.last_name;
                 });
-                const main_content = "User Details for " + new_user.first_name + " " + new_user.last_name;
-            });
+        //}
     }
 
-  render() {
-        const {user} = this.state;
 
+
+
+    render() {
+        const {user} = this.state;
         if(!user){
             return null;
         }
 
     const photosLink = "#/photos/" + user._id;
-
     return (
         <div className = "user-detail">
+            <p></p>
             <Button href={photosLink}>User Photos</Button>
             <p className={"PageOwner"}>{user.first_name + " " + user.last_name + "'s Profile"}</p>
             <Typography variant = {'caption'} className={"User-ID"}>{`User ID: ${user._id}`}</Typography>
--- Project/06/webServer.js
+++ Project/06/webServer.js
@@ -43,6 +43,7 @@ const app = express();
 const User = require("./schema/user.js");
 const Photo = require("./schema/photo.js");
 const SchemaInfo = require("./schema/schemaInfo.js");
+const ObjectId = require('mongodb').ObjectId;
 
 // XXX - Your submission should work without this line. Comment out or delete
 // this line for tests and before submission!
@@ -143,7 +144,7 @@ app.get("/test/:p1", function (request, response) {
 app.get("/user/list", function (request, response) {
   // Express parses the ":p1" from the URL and returns it in the request.params
   // objects.
-  console.log("/test called with param1 = ", request.params.p1);
+  console.log("/user/list called with param1 = ", request.params.p1);
   console.log("Yes")
 
   const param = request.params.p1 || "info";
@@ -167,33 +168,85 @@ app.get("/user/list", function (request, response) {
     }
 
     // We got the object - return it in JSON format.
-    console.log("SchemaInfo", info);
+    console.log("SchemaInfo List Fetch", info , "\n\n");
     response.end(JSON.stringify(info));
   });
 });
 
 
+app.get("/user/:p1", function (request, response) {
+  // Express parses the ":p1" from the URL and returns it in the request.params
+  // objects.
+  console.log("/user/:id called with param1 = ", request.params.p1);
+
+  const param = request.params.p1 || "info";
+  if (param !== null) {
+    // Fetch the SchemaInfo. There should only one of them. The query of {} will
+    // match it.
+    //HERE
+    User.find({ "_id": param},{"_id": 1, "first_name": 1, "last_name": 1, "location": 1, "description": 1, "occupation": 1}, function (err, info) {
+      if (err) {
+        // Query returned an error. We pass it back to the browser with an
+        // Internal Service Error (500) error code.
+        console.error("Error in /user/info:", err);
+        response.status(500).send(JSON.stringify(err));
+        return;
+      }
+      console.log(info);
+
+      if (info.length === 0) {
+        // Query didn't return an error but didn't find the SchemaInfo object -
+        // This is also an internal error return.
+        response.status(500).send("Missing SchemaInfo");
+        return;
+      }
+
+      // We got the object - return it in JSON format.
+      console.log("SchemaInfo Sepcific Fetch", info[0], "\n\n");
+      response.end(JSON.stringify(info[0]));
+    });
+  } else if (param === "counts") {
+    // In order to return the counts of all the collections we need to do an
+    // async call to each collections. That is tricky to do so we use the async
+    // package do the work. We put the collections into array and use async.each
+    // to do each .count() query.
+    const collections = [
+      { name: "user", collection: User },
+      { name: "photo", collection: Photo },
+      { name: "schemaInfo", collection: SchemaInfo },
+    ];
+    async.each(
+        collections,
+        function (col, done_callback) {
+          col.collection.countDocuments({}, function (err, count) {
+            col.count = count;
+            done_callback(err);
+          });
+        },
+        function (err) {
+          if (err) {
+            response.status(500).send(JSON.stringify(err));
+          } else {
+            const obj = {};
+            for (let i = 0; i < collections.length; i++) {
+              obj[collections[i].name] = collections[i].count;
+            }
+            response.end(JSON.stringify(obj));
+          }
+        }
+    );
+  } else {
+    // If we know understand the parameter we return a (Bad Parameter) (400)
+    // status.
+    response.status(400).send("Bad param " + param);
+  }
+});
+
 
 /**
  * URL /user/list - Returns all the User objects.
  */
-app.get("/user/list", function (request, response) {
-  response.status(200).send(models.userListModel());
-});
 
-/**
- * URL /user/:id - Returns the information for User (id).
- */
-app.get("/user/:id", function (request, response) {
-  const id = request.params.id;
-  const user = models.userModel(id);
-  if (user === null) {
-    console.log("User with _id:" + id + " not found.");
-    response.status(400).send("Not found");
-    return;
-  }
-  response.status(200).send(user);
-});
 
 /**
  * URL /photosOfUser/:id - Returns the Photos for User (id).
