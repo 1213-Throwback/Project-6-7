2873df76338b083f27e8cda8823b0634938033a7
DJDooley <112581503+DJDooley@users.noreply.github.com>
2023-10-30 20:27:32 -0400

    Added skeleton code for the Login component

--- Project/07/components/LoginRegister/LoginRegister.jsx
+++ Project/07/components/LoginRegister/LoginRegister.jsx
@@ -1,1 +1,36 @@
+import React from 'react';
+import {
+    AppBar, Toolbar, Typography
+} from '@mui/material';
+import './LoginRegister.css';
+import axios from 'axios';
 
+class LoginRegister extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            loggedIn: false,
+            firstName: '',
+        };
+    }
+
+    handleLogin = () => {}
+
+    handleLogout = () => {}
+
+
+    render(){
+        if(this.state.loggedIn){
+            return(
+                <div>
+                    <p>Hi {this.state.firstName}</p>
+                    <button onClick = {this.handleLogout}>Logout</button>
+                </div>
+            );
+        }
+        else {
+            return <Redirect to = "/login-register" />
+        }
+    }
+}
